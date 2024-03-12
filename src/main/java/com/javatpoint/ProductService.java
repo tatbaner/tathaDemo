ac2d90f587feebcb08c7bd04207683f44ddacdb9
tatbaner <tathagata.banerjee@capgemini.com>
2024-03-12 17:09:16 +0530

    test commit for trigger jenkins build VII

--- src/main/java/com/javatpoint/ProductService.java
+++ src/main/java/com/javatpoint/ProductService.java
@@ -17,7 +17,7 @@ products.add(new Product(102, "Washing Machine", "38753BK9", 9000.00, 7));
 products.add(new Product(103, "Laptop", "LHP29OCP", 24000.00, 1));
 products.add(new Product(104, "Air Conditioner", "ACLG66721", 30000.00, 5));
 products.add(new Product(105, "Refrigerator ", "12WP9087", 10000.00, 4));
-//test
+
 return products;
 }
 }
